162463ad7411cc80903d10f042f432a5bc349396
Candan Tombas <124479676+caglarop@users.noreply.github.com>
2024-01-16 09:39:45 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ import { prisma } from "./prisma";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "secretkey123",
   providers: [
     /*
     GithubProvider({
